d3a6d1a074b9108e4a629f4ffeda72cec3d514ee
Thibault Zanini <thibaultzanini@gmail.com>
2020-08-27 16:05:11 +0200

    Add more properties to the outline button

--- lib/widgets/buttons/outline_button.dart
+++ lib/widgets/buttons/outline_button.dart
@@ -6,11 +6,13 @@ class FastOutlineButton extends StatelessWidget {
   final VoidCallback onTap;
   final String text;
   final bool isEnabled;
+  final Color textColor;
   final Color borderColor;
   final Color highlightColor;
   final Color highlightedBorderColor;
   final EdgeInsetsGeometry padding;
   final FastButtonEmphasis emphasis;
+  final Widget child;
 
   const FastOutlineButton({
     Key key,
@@ -22,13 +24,14 @@ class FastOutlineButton extends StatelessWidget {
     this.padding,
     this.emphasis = FastButtonEmphasis.low,
     this.highlightedBorderColor,
+    this.child,
+    this.textColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     final _color = borderColor ?? emphasis == FastButtonEmphasis.high
-        ? theme.primaryColor
+        ? ThemeHelper.colors.getPrimaryColor(context)
         : ThemeHelper.texts.getButtonTextStyle(context).color;
 
     final disabledColor = _color.withAlpha(155);
@@ -37,16 +40,16 @@ class FastOutlineButton extends StatelessWidget {
       child: OutlineButton(
         padding: padding,
         onPressed: isEnabled ? onTap : null,
-        borderSide: BorderSide(
-          color: _color,
-        ),
+        borderSide: BorderSide(color: _color),
         disabledBorderColor: disabledColor,
+        textColor: textColor ?? _color,
         highlightColor: highlightColor,
         highlightedBorderColor: highlightedBorderColor ?? disabledColor,
-        child: FastButtonLabel(
-          text: text,
-          textColor: isEnabled ? _color : disabledColor,
-        ),
+        child: child ??
+            FastButtonLabel(
+              text: text,
+              textColor: isEnabled ? _color : disabledColor,
+            ),
       ),
     );
   }
