42c61d9830cbed5cbf6d65bf8a99225925f9c8e1
Thibault Zanini <thibaultzanini@gmail.com>
2020-09-28 15:13:07 +0200

    update light theme

--- lib/themes/light/light_colors.dart
+++ lib/themes/light/light_colors.dart
@@ -5,14 +5,14 @@ import 'package:flutter/cupertino.dart';
 const kFastLightWhiteColor = Color(0xFFFAFAFA);
 const kFastLightBlueColor = Color(0xFF4094E3);
 const kFastLightBlueGrayColor = Color(0xFF506670);
-final kFastLightGreenColor = CupertinoColors.systemGreen.color;
-final kFastLightIndigoColor = Color(0xFF8280FF);
-const kFastLightOrangeColor = Color(0xFFFFA526);
-final kFastLightPinkColor = CupertinoColors.systemPink.color;
+const kFastLightGreenColor = Color(0xFF3CA469);
+const kFastLightIndigoColor = Color(0xFF8280FF);
+const kFastLightOrangeColor = Color(0xFFFEAE2E);
 const kFastLightPurpleColor = Color(0xFFC95EFF);
-const kFastLightRedColor = Color(0xFFE34A19);
+const kFastLightRedColor = Color(0xFFF23B27);
+const kFastLightYellowColor = Color(0xFFFCD93A);
+final kFastLightPinkColor = CupertinoColors.systemPink.color;
 final kFastLightTealColor = CupertinoColors.systemTeal.color;
-final kFastLightYellowColor = Color(0xFFFCD93A);
 
 // Background Colors
 const kFastLightPrimaryBackgroundColor = kFastLightWhiteColor;
@@ -28,4 +28,4 @@ final kFastLightHintLabelColor = CupertinoColors.placeholderText.color;
 const kFastLightSeparatorColor = Color(0xFFE0E0E0);
 
 // Shawdow Colors
-final kFastLightShadowColor = kFastLightlabelColor.withOpacity(0.3);
+final kFastLightShadowColor = kFastLightTertiaryLabelColor.withOpacity(0.2);
