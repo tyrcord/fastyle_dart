507081df35a8fd63b030c3de4d128a3d09695d7f
Thibault Zanini <thibaultzanini@gmail.com>
2020-09-28 15:18:02 +0200

    Workaround material specs for the tile widget

--- lib/layouts/list_item.layout.dart
+++ lib/layouts/list_item.layout.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:fastyle_dart/fastyle_dart.dart';
 
-final _kTransform = Matrix4.translationValues(-16, 0.0, 0.0);
-
-class FastListItemLayout extends StatelessWidget {
+class FastListItemLayout extends StatefulWidget {
   final FastListItemDescriptor descriptor;
   final EdgeInsets contentPadding;
   final String descriptionText;
@@ -31,30 +29,79 @@ class FastListItemLayout extends StatelessWidget {
         super(key: key);
 
   @override
+  _FastListItemLayoutState createState() => _FastListItemLayoutState();
+}
+
+class _FastListItemLayoutState extends State<FastListItemLayout> {
+  GlobalKey _leadingKey = GlobalKey();
+  bool _isInitialized = false;
+  double _leadingOffsetX;
+  Widget _leading;
+
+  // Material specifications.
+  static const double _minLeadingWidth = 40.0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (!_isInitialized) {
+      _leading = widget.leading ?? widget.descriptor?.leading;
+      _isInitialized = true;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final title = FastBody(text: titleText);
+    final title = FastBody(text: widget.titleText);
+    Matrix4 transform;
+    Widget subtitle;
+
+    if (_leadingOffsetX != null) {
+      transform = Matrix4.translationValues(-_leadingOffsetX, 0.0, 0.0);
+    }
+
+    if (widget.descriptionText != null) {
+      subtitle = FastBody2(text: widget.descriptionText);
+
+      if (transform != null) {
+        subtitle = Transform(transform: transform, child: subtitle);
+      }
+    }
 
     return ThemeHelper.getListTitleTheme(
       context: context,
       child: ListTile(
-        contentPadding: contentPadding,
-        dense: isDense ?? descriptor?.isDense ?? true,
-        enabled: isEnabled,
-        leading: leading ?? descriptor?.leading,
-        title: leading != null
-            ? Transform(transform: _kTransform, child: title)
-            : title,
-        subtitle: descriptionText != null
-            ? leading == null
-                ? FastBody2(text: descriptionText)
-                : Transform(
-                    transform: _kTransform,
-                    child: FastBody2(text: descriptionText),
-                  )
+        contentPadding: widget.contentPadding,
+        dense: widget.isDense ?? widget.descriptor?.isDense ?? true,
+        enabled: widget.isEnabled,
+        leading: _leading != null
+            ? Container(key: _leadingKey, child: _leading)
             : null,
-        trailing: trailing ?? descriptor?.trailing,
-        onTap: onTap,
+        title: transform != null
+            ? Transform(transform: transform, child: title)
+            : title,
+        subtitle: subtitle,
+        trailing: widget.trailing ?? widget.descriptor?.trailing,
+        onTap: widget.onTap,
       ),
     );
+  }
+
+  _afterLayout(_) {
+    if (_leading != null) {
+      final RenderBox renderBox = _leadingKey.currentContext.findRenderObject();
+      final width = renderBox.size.width;
+
+      if (width < _minLeadingWidth) {
+        setState(() => _leadingOffsetX = _minLeadingWidth - width);
+      }
+    }
   }
 }
