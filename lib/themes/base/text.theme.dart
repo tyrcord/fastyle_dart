db464352ada83a2b2bf3ed1343c2155b289fc06c
Thibault Zanini <thibaultzanini@gmail.com>
2020-11-24 15:52:54 +0100

    Add default font family fallbacks

--- lib/themes/base/text.theme.dart
+++ lib/themes/base/text.theme.dart
@@ -8,45 +8,53 @@ const _kFontSize18 = 18.0;
 const _kFontSize16 = 16.0;
 const _kFontSize14 = 14.0;
 
+const _kFontFamilyFallback = <String>[
+  'San Francisco',
+  'Helvetica Neue',
+  'Helvetica',
+  'Roboto',
+  'Arial',
+];
+
 final kFastTextTheme = TextTheme(
   headline4: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize48,
     fontWeight: FontWeight.w400,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   headline5: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize34,
     fontWeight: FontWeight.w700,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   subtitle1: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize18,
     fontWeight: FontWeight.w700,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   headline6: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize24,
     fontWeight: FontWeight.w500,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   subtitle2: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize16,
     fontWeight: FontWeight.w700,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   bodyText1: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize18,
     fontWeight: FontWeight.w400,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   bodyText2: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize16,
     fontWeight: FontWeight.w400,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   button: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize16,
     fontWeight: FontWeight.w700,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   caption: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize16,
     fontWeight: FontWeight.w400,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
   overline: GoogleFonts.robotoCondensed(
     fontSize: _kFontSize14,
     fontWeight: FontWeight.w400,
-  ),
+  ).copyWith(fontFamilyFallback: _kFontFamilyFallback),
 );
