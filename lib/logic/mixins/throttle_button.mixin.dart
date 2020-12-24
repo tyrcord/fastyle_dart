a2362bec96ce7fc230dd5eb4e311846afe67f110
Thibault Zanini <thibaultzanini@gmail.com>
2020-12-24 19:28:25 +0100

    clean up

--- lib/logic/mixins/throttle_button.mixin.dart
+++ lib/logic/mixins/throttle_button.mixin.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
-import 'package:fastyle_dart/fastyle_dart.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
+
+import 'package:fastyle_dart/fastyle_dart.dart';
 
 mixin FastThrottleButtonMixin<T extends IFastButton> on State<T> {
   @protected
