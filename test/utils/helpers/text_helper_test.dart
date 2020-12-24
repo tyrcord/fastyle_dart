e83dfdd6c0da41cfc2876a4160c33acc7e159a59
Thibault Zanini <thibaultzanini@gmail.com>
2020-12-24 19:28:17 +0100

    update specs

--- test/utils/helpers/text_helper_test.dart
+++ test/utils/helpers/text_helper_test.dart
@@ -5,15 +5,18 @@ import 'package:fastyle_dart/fastyle_dart.dart';
 void main() {
   group('#normalizeTextByRemovingDiacritics()', () {
     test('should lowercase any given text', () {
-      expect(normalizeTextByRemovingDiacritics('LOWERCASE'), 'lowercase');
+      expect(
+        normalizeTextByRemovingDiacritics('LOWERCASE'),
+        equals('lowercase'),
+      );
     });
 
     test('should remove any diacritics from any given text', () {
-      expect(normalizeTextByRemovingDiacritics('brûlée'), 'brulee');
+      expect(normalizeTextByRemovingDiacritics('brûlée'), equals('brulee'));
     });
 
     test('should lowercase and remove any diacritics from any given text', () {
-      expect(normalizeTextByRemovingDiacritics('Brûlée'), 'brulee');
+      expect(normalizeTextByRemovingDiacritics('Brûlée'), equals('brulee'));
     });
   });
 }
