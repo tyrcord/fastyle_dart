fa89064c76e5c3ebfb4e5aca52213a45b867c161
Thibault Zanini <thibaultzanini@gmail.com>
2022-10-21 14:56:37 +0200

    feat: add media breakpoints

---
+++ lib/logic/models/media_breakpoints.model.dart
@@ -0,0 +1,7 @@
+class FastMediaBreakpoints {
+  static const int handset = 0;
+  static const int tablet = 600;
+  static const int desktop = 840;
+  static const int large = 1200;
+  static const int xl = 1536;
+}
--- lib/logic/models/models.dart
+++ lib/logic/models/models.dart
@@ -3,4 +3,5 @@ export './item.model.dart';
 export './job.model.dart';
 export './job_error.model.dart';
 export './list_item_category.model.dart';
+export './media_breakpoints.model.dart';
 export './notification_center_options.model.dart';
