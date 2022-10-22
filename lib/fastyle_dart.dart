bcb333bc220040aa7cd7456f6d975e764c66babb
Thibault Zanini <thibaultzanini@gmail.com>
2022-10-22 12:05:53 +0200

    fix: import

--- lib/fastyle_dart.dart
+++ lib/fastyle_dart.dart
@@ -3,7 +3,7 @@ library fastyle_dart;
 export './app.dart';
 export './app_error_reporter.dart';
 export './app_loader.dart';
-export './constants.dart';
+export './constants/constants.dart';
 
 export './logic/logic.dart';
 export './ui/ui.dart';
