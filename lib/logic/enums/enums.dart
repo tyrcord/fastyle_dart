0cfd24e4c6e228225572f27d713d29f7796c2e92
Thibault Zanini <thibaultzanini@gmail.com>
2022-11-06 19:27:32 +0100

    feat: add position enum

--- lib/logic/enums/enums.dart
+++ lib/logic/enums/enums.dart
@@ -1,3 +1,4 @@
 export './button_emphasis.enum.dart';
 export './media_type.enum.dart';
+export './position.enum.dart';
 export './section_page_load_event.enum.dart';
---
+++ lib/logic/enums/position.enum.dart
@@ -0,0 +1,6 @@
+enum FastPosition {
+  top,
+  right,
+  bottom,
+  left,
+}
