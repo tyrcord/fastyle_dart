8a6e2f2d2564fd4fffd0a599db74444a5107da5c
Thibault Zanini <thibaultzanini@gmail.com>
2022-12-30 16:03:04 +0100

    feat: add more options to palettes

--- lib/ui/themes/palette_scheme.dart
+++ lib/ui/themes/palette_scheme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FastPaletteScheme {
+  final Color ultraLight;
   final Color lightest;
   final Color lighter;
   final Color light;
@@ -8,8 +9,10 @@ class FastPaletteScheme {
   final Color dark;
   final Color darker;
   final Color darkest;
+  final Color ultraDark;
 
   const FastPaletteScheme._internal({
+    required this.ultraLight,
     required this.lightest,
     required this.lighter,
     required this.light,
@@ -17,12 +20,14 @@ class FastPaletteScheme {
     required this.dark,
     required this.darker,
     required this.darkest,
+    required this.ultraDark,
   });
 
   factory FastPaletteScheme.fromColor(Color color) {
     final base = HSLColor.fromColor(color);
 
     return FastPaletteScheme._internal(
+      ultraLight: base.withLightness(0.90).toColor(),
       lightest: base.withLightness(0.84).toColor(),
       lighter: base.withLightness(0.73).toColor(),
       light: base.withLightness(0.62).toColor(),
@@ -30,6 +35,7 @@ class FastPaletteScheme {
       dark: base.withLightness(0.42).toColor(),
       darker: base.withLightness(0.33).toColor(),
       darkest: base.withLightness(0.24).toColor(),
+      ultraDark: base.withLightness(0.1).toColor(),
     );
   }
 }
