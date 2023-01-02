cac1fc0f5f829404d0136d8f2b65614a5ad6008f
Thibault Zanini <thibaultzanini@gmail.com>
2023-01-02 20:01:16 +0100

    remove workaround

--- lib/ui/layouts/list_item.layout.dart
+++ lib/ui/layouts/list_item.layout.dart
@@ -2,7 +2,7 @@ import 'package:fastyle_dart/fastyle_dart.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class FastListItemLayout extends StatefulWidget {
+class FastListItemLayout extends StatelessWidget {
   ///
   /// Describes some additional visual aspects of an item.
   ///
@@ -74,101 +74,36 @@ class FastListItemLayout extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _FastListItemLayoutState createState() => _FastListItemLayoutState();
-}
-
-class _FastListItemLayoutState extends State<FastListItemLayout> {
-  final _leadingKey = GlobalKey();
-  late final Widget? _leading;
-  bool _isInitialized = false;
-  double? _leadingOffsetX;
-
-  bool get isDense => widget.descriptor?.isDense ?? widget.isDense;
-
-  // Material specifications.
-  static const _minLeadingWidth = 40.0;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    if (!_isInitialized) {
-      _leading = widget.descriptor?.leading ?? widget.leading;
-      _isInitialized = true;
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    Matrix4? transform;
     Widget? subtitle;
 
-    if (_leadingOffsetX != null) {
-      transform = Matrix4.translationValues(-_leadingOffsetX!, 0.0, 0.0);
-    }
-
-    if (widget.descriptionText != null) {
-      subtitle = FastSecondaryBody(text: widget.descriptionText!);
-
-      if (transform != null) {
-        subtitle = Transform(transform: transform, child: subtitle);
-      }
+    if (descriptionText != null) {
+      subtitle = FastSecondaryBody(text: descriptionText!);
     }
 
     return ThemeHelper.getListTitleTheme(
       context: context,
       child: Container(
-        color: widget.selectionColor,
+        color: selectionColor,
         child: ListTile(
-          trailing: widget.descriptor?.trailing ?? widget.trailing,
-          contentPadding: widget.contentPadding,
-          title: _buildTitle(transform),
-          enabled: widget.isEnabled,
-          leading: _buildLeading(),
-          onTap: widget.onTap,
+          trailing: descriptor?.trailing ?? trailing,
+          leading: descriptor?.leading ?? leading,
+          dense: descriptor?.isDense ?? isDense,
+          contentPadding: contentPadding,
+          minLeadingWidth: 16.0,
+          title: _buildTitle(),
           subtitle: subtitle,
-          dense: isDense,
+          enabled: isEnabled,
+          onTap: onTap,
         ),
       ),
     );
   }
 
-  Widget _buildTitle(Matrix4? transform) {
-    final labelText = widget.capitalizeLabelText
-        ? toBeginningOfSentenceCase(widget.labelText)!
-        : widget.labelText;
+  Widget _buildTitle() {
+    final _labelText =
+        capitalizeLabelText ? toBeginningOfSentenceCase(labelText)! : labelText;
 
-    final title = FastBody(
-      textColor: widget.selectionLabelColor,
-      text: labelText,
-    );
-
-    return transform != null
-        ? Transform(transform: transform, child: title)
-        : title;
-  }
-
-  Widget? _buildLeading() {
-    return _leading != null
-        ? Container(key: _leadingKey, child: _leading)
-        : null;
-  }
-
-  void _afterLayout(_) {
-    if (_leading != null && _leadingKey.currentContext != null) {
-      final renderBox =
-          _leadingKey.currentContext!.findRenderObject() as RenderBox;
-      final width = renderBox.size.width;
-
-      if (width < _minLeadingWidth) {
-        setState(() => _leadingOffsetX = _minLeadingWidth - width);
-      }
-    }
+    return FastBody(textColor: selectionLabelColor, text: _labelText);
   }
 }
